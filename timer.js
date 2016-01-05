05975d92860bc24018162eaf957904ca4914dfec
joeyaza <JAzadehdel@gmail.com>
2016-01-05 20:35:05 +0000

    first commit

---
+++ timer.js
@@ -0,0 +1,31 @@
+// var timeoutID;
+
+// function delayedAlert() {
+//   timeoutID = window.setTimeout(slowAlert, 5000);
+// }
+
+// function slowAlert() {
+//   alert("Your time is up!");
+
+// }
+
+// function clearAlert() {
+//   window.clearTimeout(timeoutID);
+// }
+
+// delayedAlert();
+// slowAlert();
+// clearAlert();
+
+
+var scoreBoard = $('li').click(function() { count++;
+  $("#score").html("Christmas count is: "+count); })
+
+  $('li').on("click", function() {
+    scoreBoard;  
+
+  })
+
+  
+
+
